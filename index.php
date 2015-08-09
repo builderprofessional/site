262e60f9b1e3c8da5335c980875a957f078ba208
root <root@vm1.builderprofessional.com>
2015-08-09 20:13:57 +0000

    initial bootstrap

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="/boostrap/js/boostrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<h1>Yo</h1>
+			<p>Some Text</p>
+		</div>
+	</body>
+</html>
