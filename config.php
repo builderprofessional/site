1dfca3a4cf88fe6c70883eb67a6b1ed1242a43d9
root <root@vm1.builderprofessional.com>
2015-08-19 18:34:14 -0500

    working our values page but with bad content

--- config.php
+++ config.php
@@ -3,5 +3,7 @@ $config = [];
 
 $config['page_name'] = basename($_SERVER['PHP_SELF']);
 $config['twitter'] = 'https://twitter.com/wehelpbuilders';
+$config['facebook'] = 'https://www.facebook.com/wehelpbuilders';
+$config['linkedin'] = 'https://www.linkedin.com/company/builder-professional';
 
 global $config;
---
+++ js/ourvalues.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('.value_link, .value_read_more a').mouseover(function() {
+    $(this).closest('.value_wrapper').find('span.circle').addClass('value_circle_highlight');
+    $(this).closest('.value_wrapper').find('h4').addClass('value_title_highlight');
+    $(this).closest('.value_wrapper').find('.value_read_more').find('span').addClass('value_title_highlight');
+  });
+
+  $('.value_link, .value_read_more a').mouseout(function() {
+    $(this).closest('.value_wrapper').find('span.circle').removeClass('value_circle_highlight');
+    $(this).closest('.value_wrapper').find('h4').removeClass('value_title_highlight');
+    $(this).closest('.value_wrapper').find('.value_read_more').find('span').removeClass('value_title_highlight');
+  });
+
+  $('.value_link, .value_read_more a').click(function() {
+    var wrapper = $(this).closest('.value_wrapper');
+
+    $('.value_detail').fadeOut(function() {
+      $(this).find('h1').html(wrapper.find('.value_long_title').html());
+      $(this).find('.value_detail_description').html(wrapper.find('.value_description').html());
+    }).fadeIn();
+  });
+});
