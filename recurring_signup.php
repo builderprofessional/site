b501e5dac8857087e4a863b1ec1ff02cb595dd95
Tony Vance <tony.vance@builderprofessional.com>
2015-10-17 10:26:54 -0500

    latest progress

--- recurring_signup.php
+++ recurring_signup.php
@@ -1,3 +1,12 @@
 <?php
 require_once('setup.php');
-start_page('recurring_signup.twig');
+
+global $conn;
+
+$query = "SELECT * FROM recurring_plan WHERE id=%s";
+$query = sprintf($query, mysql_real_escape_string($_REQUEST['plan_id']));
+
+$dbResult = mysql_query($query);
+$plan = mysql_fetch_assoc($dbResult);
+
+start_page('recurring_signup.twig', ['plan' => $plan]);
